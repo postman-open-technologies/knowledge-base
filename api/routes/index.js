ecd8fedff70e02496cbb48385f9e640316524529
Bruno Pedro <bpedro@brunopedro.com>
2023-01-24 12:54:24 +0100

    Negotiate root content based on the Accept header

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,10 +1,18 @@
+const fs = require('fs');
 const express = require('express');
 const router = express.Router();
 
 const openapi = require('../openapi.json');
 
 router.get('/', function(req, res, next) {
-  res.json(openapi);
+  res.format({
+    json: function() {
+      res.json(openapi);
+    },
+    html: function() {
+      res.send(fs.readFileSync('openapi.html', 'utf-8'));
+    }
+  });
 });
 
 module.exports = router;
