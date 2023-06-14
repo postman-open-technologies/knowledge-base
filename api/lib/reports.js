5e84b76c333e23b16f0550a09f38e0475098bb7a
Bruno Pedro <bpedro@brunopedro.com>
2023-06-14 10:36:26 +0200

    Replace queries to use a materialized view

--- api/lib/reports.js
+++ api/lib/reports.js
@@ -2,11 +2,10 @@ const db = require('./db');
 
 module.exports.all = async () => {
   result = await db.query(`
-  SELECT DISTINCT attachment_type, code
-  FROM vw_oas_resource_spectral_errors
-  WHERE attachment_type LIKE 'spectral/%'
-  AND code NOT IN ('invalid-ref', 'parser','unrecognized-format')
-  ORDER BY 1,2
+  SELECT *
+  FROM mat_vw_oas_resource_spectral_rules
+  WHERE code NOT IN ('invalid-ref', 'parser','unrecognized-format')
+  ORDER BY code, attachment_type
   `);
   return result.rows.map(row => {
     return {
@@ -18,12 +17,10 @@ module.exports.all = async () => {
 
 module.exports.get = async (id) => {
   result = await db.query(`
-  SELECT DISTINCT attachment_type, code
-  FROM vw_oas_resource_spectral_errors
-  WHERE attachment_type LIKE 'spectral/%'
-  AND code = $1
+  SELECT *
+  FROM mat_vw_oas_resource_spectral_rules
+  WHERE code = $1
   AND code NOT IN ('invalid-ref', 'parser','unrecognized-format')
-  ORDER BY 1,2
   `, [id]);
   return result.rowCount && result.rows.map(row => {
     return {
